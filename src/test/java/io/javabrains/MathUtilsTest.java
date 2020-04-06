c1a3444d9728daa688a759f2f69512e98b7605ff
pavan kumar <pavankumar@scmgalaxy.com>
2020-04-05 20:17:25 -0400

    Nested class and using assertAll

--- src/test/java/io/javabrains/MathUtilsTest.java
+++ src/test/java/io/javabrains/MathUtilsTest.java
@@ -1,14 +1,66 @@
 package io.javabrains;
-
 import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assumptions.*;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.TestInstance.Lifecycle;
 
-import org.junit.jupiter.api.Test;
-
+//@TestInstance(Lifecycle.PER_CLASS)
+@DisplayName("When running MathUtils")
 class MathUtilsTest {
+	
+	MathUtils mathUtils; //member varaible
+	
+	@BeforeAll
+	static void beforeAllInit() {
+		System.out.println("This needs to run before all");
+	}
+	
+	/*@BeforeAll
+	void beforeAllInit() {  //when u use @TestInstance(Lifecycle.PER_CLASS)
+		System.out.println("This needs to run before all");
+	}*/
+	
+	@BeforeEach
+	void init() {
+		mathUtils = new MathUtils(); //Instance is created
+	}
+	
+	@AfterEach
+	void cleanup() {
+		System.out.println("Clenaing up...");
+	}
 
+	@Nested
+	@DisplayName("add method")
+	class AddTest{
+		
+		@Test
+		@DisplayName("when adding two positive numbers")
+		void testAddPositive() {
+			assertEquals(2, mathUtils.add(1, 1), "should return the right sum");
+		}
+		
+		@Test
+		@DisplayName("when adding two negative numbers")
+		void testAddNegative() {
+			assertEquals(-2, mathUtils.add(-1, -1), "should return the right sum");
+		}
+	}
+	
 	@Test
-	void testAdd() {
-		MathUtils mathUtils = new MathUtils();
+	@DisplayName("multiple method")
+	void testMultiply() {
+		//In one shot we passing 3 asserts
+		assertAll(
+				() -> assertEquals(4, mathUtils.multiply(2, 2)),
+				() -> assertEquals(0, mathUtils.multiply(2, 0)),
+				() -> assertEquals(-2, mathUtils.multiply(2, -1))
+			);
+	}
+	
+	@Test
+	@DisplayName("when adding two positive numbers")
+	void testAddPositive() {
 		int expectedVal = 2;
 		int actualVal = mathUtils.add(1, 1);
 		assertEquals(expectedVal, actualVal, "The add method should add two numbers");
@@ -16,18 +68,23 @@ class MathUtilsTest {
 	
 	@Test
 	void testComputeCircleRadius() {
-		MathUtils mathUtils = new MathUtils();
 		//Area of circle for radius 10 is 314.1592653589793
 		assertEquals(314.1592653589793, mathUtils.computeCircleArea(10), "Should return right circle area");
 	}
 	
 	@Test
 	void testdivide() {
-		MathUtils mathUtils = new MathUtils();
+		boolean isServerUp = false;
+		assumeTrue(isServerUp);
 		//assertThrows(NullPointerException.class, () -> mathUtils.divide(1, 0), "Divide by zero show throw"); //this will fail
 		assertThrows(ArithmeticException.class, () -> mathUtils.divide(1, 0), "Divide by zero show throw"); // this will pass
 	}
 	
-	
+	@Test
+	@Disabled
+	@DisplayName("TDD method . should not run")
+	void testDisabled() {
+		fail("This test should be disabled");
+	}
 
 }
